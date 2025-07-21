53989a6817cb742b40f81ed69a493c485faf4802
Ram Yadav <yram75219@gmail.com>
2025-07-21 17:09:17 +0545

    Portfolio updated

--- app/public/wp-content/themes/flex-persona/inc/woocommerce.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * functions and hooks for woocommerce
- *
- * @package wpopus_fse
- */
-
-// remove woocommerce core breadcrumb
-remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
